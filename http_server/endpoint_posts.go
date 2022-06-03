642674a5b09f3657dcf11e84b27134f222928486
Vlad Romanciuc <vladislavromanciuc@gmail.com>
2022-06-03 21:30:49 +0000

    to main

--- http_server/endpoint_posts.go
+++ http_server/endpoint_posts.go
@@ -1,94 +1,19 @@
-package database
+package main
 
 import (
 	"errors"
-	"time"
+	"net/http"
 )
 
-// User -
-type User struct {
-	CreatedAt time.Time `json:"createdAt"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Name      string    `json:"name"`
-	Age       int       `json:"age"`
-}
-
-// CreateUser -
-func (c Client) CreateUser(
-	email, password, name string,
-	age int,
-) (User, error) {
-	db, err := c.readDB()
-	if err != nil {
-		return User{}, err
+func (apiCfg apiConfig) endpointPostsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		apiCfg.handlerRetrievePosts(w, r)
+	case http.MethodPost:
+		apiCfg.handlerCreatePost(w, r)
+	case http.MethodDelete:
+		apiCfg.handlerDeletePost(w, r)
+	default:
+		respondWithError(w, 404, errors.New("method not supported"))
 	}
-	if _, ok := db.Users[email]; ok {
-		return User{}, errors.New("user already exists")
-	}
-	user := User{
-		CreatedAt: time.Now().UTC(),
-		Email:     email,
-		Password:  password,
-		Name:      name,
-		Age:       age,
-	}
-	db.Users[email] = user
-
-	err = c.updateDB(db)
-	if err != nil {
-		return User{}, err
-	}
-	return user, err
-}
-
-// UpdateUser -
-func (c Client) UpdateUser(
-	email, password, name string,
-	age int,
-) (User, error) {
-	db, err := c.readDB()
-	if err != nil {
-		return User{}, err
-	}
-	user, ok := db.Users[email]
-	if !ok {
-		return User{}, errors.New("user doesn't exist")
-	}
-	user.Password = password
-	user.Name = name
-	user.Age = age
-	db.Users[email] = user
-	err = c.updateDB(db)
-	if err != nil {
-		return User{}, err
-	}
-	return user, err
-}
-
-// GetUser -
-func (c Client) GetUser(email string) (User, error) {
-	db, err := c.readDB()
-	if err != nil {
-		return User{}, err
-	}
-	user, ok := db.Users[email]
-	if !ok {
-		return User{}, errors.New("user doesn't exist")
-	}
-	return user, nil
-}
-
-// DeleteUser -
-func (c Client) DeleteUser(email string) error {
-	db, err := c.readDB()
-	if err != nil {
-		return err
-	}
-	delete(db.Users, email)
-	err = c.updateDB(db)
-	if err != nil {
-		return err
-	}
-	return nil
 }
