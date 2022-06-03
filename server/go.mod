31206517a1b0a17d12e23e9c3c51295b8e22aa75
Vlad Romanciuc <vladislavromanciuc@gmail.com>
2022-06-03 18:51:08 +0200

    to main

--- server/go.mod
+++ server/go.mod
@@ -3,3 +3,4 @@ module http_server
 go 1.18
 
 require github.com/google/uuid v1.3.0 // indirect
+require github.com/VladRomanciuc/Go-classes/blob/7c5e94157479cd8bc5cb9ea2a5345b40f3e76e96/server/internal/database
--- server/main.go
+++ server/main.go
@@ -1,29 +1,23 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/VladRomanciuc/Go-classes/blob/7c5e94157479cd8bc5cb9ea2a5345b40f3e76e96/server/internal/database"
 )
 
-//func to set header and type json
-func testHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write([]byte("{}"))
-}
-
-
 func main() {
-	//set the handler as ServeMux
 	m := http.NewServeMux()
-	m.HandleFunc("/", testHandler)
 
-	//set the port
+	m.HandleFunc("/", testHandler)
+	m.HandleFunc("/err", testErrHandler)
+
 	const addr = "localhost:8080"
-	
-	//define the server
 	srv := http.Server{
 		Handler:      m,
 		Addr:         addr,
@@ -31,12 +25,53 @@ func main() {
 		ReadTimeout:  30 * time.Second,
 	}
 
-	//print the info before start the server
+	// this blocks forever, until the server
+	// has an unrecoverable error
 	fmt.Println("server started on ", addr)
-	
-	//start the server with error handler
 	err := srv.ListenAndServe()
 	log.Fatal(err)
-
 }
 
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithJSON(w, 200, database.User{
+		Email: "test@example.com",
+	})
+}
+
+func testErrHandler(w http.ResponseWriter, r *http.Request) {
+	respondWithError(w, 500, errors.New("server error"))
+}
+
+type errorBody struct {
+	Error string `json:"error"`
+}
+
+func respondWithError(w http.ResponseWriter, code int, err error) {
+	if err == nil {
+		log.Println("don't call respondWithError with a nil err!")
+		return
+	}
+	log.Println(err)
+	respondWithJSON(w, code, errorBody{
+		Error: err.Error(),
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	if payload != nil {
+		response, err := json.Marshal(payload)
+		if err != nil {
+			log.Println("error marshalling", err)
+			w.WriteHeader(500)
+			response, _ := json.Marshal(errorBody{
+				Error: "error marshalling",
+			})
+			w.Write(response)
+			return
+		}
+		w.WriteHeader(code)
+		w.Write(response)
+	}
+}
