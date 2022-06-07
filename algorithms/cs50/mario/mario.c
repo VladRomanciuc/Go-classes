eb740abfbcbafb732601e4f1744824e9375b6f98
Vlad Romanciuc <vladislavromanciuc@gmail.com>
2022-06-07 21:50:09 +0200

    to main

--- algorithms/cs50/mario/mario.c
+++ algorithms/cs50/mario/mario.c
@@ -1,6 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
-
+//task: build:
+//        #
+//       ##
+//      ###
+//     ####
+//    #####
+//   ######
+//  #######
+// ########
 
 int main(void)
 {
@@ -8,22 +16,26 @@ int main(void)
 for (int g = 0; g >= 0; g++)
 {
     int input = get_int("Enter a number between 1 and 8:");
-
+        //check if entry is between 1 and 8
     if (input >= 1 && input <= 8)
     {
      int line = 1;
+     //builder
      while (line <= input)
      {
      int space = input - line;
+     //print empty spaces
         for (int sp = space; sp >0; sp--)
 {
         printf(" ");
-};
+};   
+     //print #
         for (int i = input; i > space; i--)
 {
         printf("#");
 };
      printf("\n");
+     //go to next line
      line++;
     }
     }
