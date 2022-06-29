b2ce0e60ef604f1e0ae2fda7050e73b530eb455a
Vlad Romanciuc <vladislavromanciuc@gmail.com>
2022-06-29 16:53:18 +0200

    to main

--- api/main.go
+++ api/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/VladRomanciuc/Go-classes/api/controller"
-    //"github.com/VladRomanciuc/Go-classes/api/model"
+	"log"
+	"net/http"
+
+	"github.com/VladRomanciuc/Go-classes/api/controller"
+	"github.com/VladRomanciuc/Go-classes/api/model"
 )
 
 
 func main() {
-    api := controller.Register()
-    //db := model.Connect()
-    //defer db.Close()
+    api := controller.Register() 
+	model.Connect()
     log.Fatal(http.ListenAndServe(":8080", api))
 }
--- api/model/db.go
+++ api/model/db.go
@@ -1,12 +1,49 @@
 package model
 
 import (
-
+	"log"
+	"github.com/spf13/viper"
+	f "github.com/fauna/faunadb-go/v4/faunadb"
 	
 )
 
+func viperEnv(key string) string {
+	
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	  }
+	value, ok := viper.Get(key).(string)
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	  }
+	
+	return value
+}
 
-func Connect()  {
-		
+var (
+	key = viperEnv("FAUNA_KEY")
+	endPoint = f.Endpoint("https://db.eu.fauna.com:443")
+	adminClient = f.NewFaunaClient(key, endPoint)
+	dbName = "api-go"
+)
+
+func Connect() {
+	res, err := adminClient.Query(
+		f.If(
+			f.Exists(f.Database(dbName)),
+			true,
+			f.CreateDatabase(f.Obj{"name": dbName})))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if res != f.BooleanV(true) {
+		log.Printf("Created Database: %s\n %s", dbName, res)
+	} else {
+		log.Printf("Database: %s, Already Exists\n %s", dbName, res)
+	}
 }
 
